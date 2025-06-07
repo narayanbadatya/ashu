4ed1d16c204cfa1a53fab6c67a82432b70e56c38
NARAYAN <narayanbadatya129@gmail.com>
2025-06-07 09:46:09 +0530

    Added untracked files

---
+++ src/constants.js
@@ -0,0 +1,317 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.webp';
+import canvaLogo from './assets/tech_logo/canva.png';
+
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/sparsh.jpg';
+import vpsLogo from './assets/education_logo/svm.jpg';
+import nodalLogo from './assets/education_logo/nodal.jpg';
+import roshLogo from './assets/education_logo/rosh.jpeg';
+import gvLogo from './assets/education_logo/graduvation.jpg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+     
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+     
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+     
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+    
+      { name: 'JavaScript', logo: javascriptLogo },
+     
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+     
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Photoshop', logo: firebaseLogo },
+      { name: 'Canva', logo: canvaLogo },
+      { name: 'Capcut', logo: csharpLogo },
+      
+      
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: " name",
+      company: "Webverse Digital",
+      date: "date",
+      desc: "desc",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+      
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: " ",
+      company: "name of company",
+      date: "date",
+      desc: "desc",
+      skills: [
+
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "name",
+      company: " School",
+      date: "name",
+      desc: "desc",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: gvLogo,
+      school: "Graduvation",
+      date: "date",
+      grade: "0.00 CGPA",
+      desc: "Currently learing Full-Stack Web Development using tools HTML,CSS,JavaCript,React(Front-end) and Node.js,MongoDB (Back-end). Building projects for development skills",
+      degree: "Full Stack Web Development",
+    },
+    {
+      id: 0,
+      img: roshLogo,
+      school: "school of passion",
+      date: "date",
+      desc: "I create creative Grapic Desings and professional poster and video edits that give your brand a unique identity. Bringing originality and impact to every project to help grow your business effectively",
+      degree: "Graphic Design with Marketing",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "Sparsh Collge of Computer & Management",
+      date: "date",
+      grade: "Maintaining-7.8 CGPA",
+      desc: "I am currenly pursuing BCA and I'm a second-year student and with a strong interest in web development. I've built responsive websites using HTML,CSS,JavaScript and Tailwindcss.",
+      degree: " BCA - Bachelor of Computer Application ",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Saraswati vidya Mandir",
+      date: "Apr 2021 - March 2023",
+      grade: "54.4%",
+      desc: "i completed my class 12 education from Saraswati vidya Mandir, Under the CBSE board, where i studied Economics and Education.",
+      degree: "CBSE(XII) - Arts with Economics and Education",
+    },
+    {
+      id: 3,
+      img: nodalLogo,
+      school: "Malusanta Govt. Nodal High School, Damanjodi",
+      date: " Date - 2021",
+      grade: "65.8%",
+      desc: "I completed my class 10 education form Malusnata Govt, Nodal high School, Damanjodi, under the CBSE board,where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "name",
+      description:
+        "type",
+      // image:
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 1,
+      title: "name",
+      description:
+        "type",
+      // image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "",
+      webapp: "",
+      
+    },
+    {
+      id: 2,
+      title: "name",
+      description:
+        "type",
+      // image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "",
+      webapp: "",
+     
+    },
+    {
+      id: 3,
+      title: "name",
+      description:
+        "type",
+      // image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "",
+      webapp: "",
+     
+    },
+    {
+      id: 4,
+      title: "name",
+      description:
+        "type",
+      // image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "",
+      webapp: "",
+     
+    },
+    {
+      id: 5,
+      title: "name",
+      description:
+        "type",
+      // image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "",
+      webapp: "",
+     
+    },
+    {
+      id: 6,
+      title: "name",
+      description:
+        "type",
+      // image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "",
+      webapp: "",
+      
+    },
+    {
+      id: 7,
+      title: "name",
+      description:
+        "type",
+      // image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "",
+      webapp: "",
+      
+    },
+    {
+      id: 8,
+      title: "name",
+      description:
+        "type",
+      // image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "",
+      webapp: "",
+      
+      
+    },
+  ];
