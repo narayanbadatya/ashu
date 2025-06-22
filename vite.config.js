b3d676ca7cce55fda1ed3b92e970690aeb61eef9
NARAYAN <narayanbadatya129@gmail.com>
2025-06-22 12:46:38 +0530

    🔧 Fix: clean install for rollup issue on Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react';
-            if (id.includes('framer-motion')) return 'vendor-motion';
-            return 'vendor'; // other libraries
-          }
-        },
-      },
-    },
+    outDir: 'dist',
   },
-});
+  server: {
+    port: 3000,
+  },
+})
